50eebe30864c31a2632a3bb86dded778f500113e
faramarz <37065807+newlighty@users.noreply.github.com>
2021-06-19 23:20:17 +0430

    add practice to control-flow

---
+++ control-flow/12-pracices.js
@@ -0,0 +1,13 @@
+// Hour 
+
+const Hour = 10;
+
+if (Hour >= 6 && Hour < 12) {
+   console.log('good morning');
+   }
+   else if(Hour >= 12 && Hour < 18) {
+       console.log('good afternoon');
+   }
+   else 
+       console.log('good evening');
+  
