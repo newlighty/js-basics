2ed1ff38abdd9753a92cd329c62b2d41d2ac4834
faramarz <37065807+newlighty@users.noreply.github.com>
2021-06-20 01:53:50 +0430

    ends of switch

--- control-flow/12-pracices.js
+++ control-flow/12-pracices.js
@@ -1,13 +1,32 @@
 // Hour 
-
+//if else
 const Hour = 10;
 
 if (Hour >= 6 && Hour < 12) {
-   console.log('good morning');
-   }
-   else if(Hour >= 12 && Hour < 18) {
-       console.log('good afternoon');
-   }
-   else 
-       console.log('good evening');
-  
+    console.log('good morning');
+} else if (Hour >= 12 && Hour < 18) {
+    console.log('good afternoon');
+} else
+    console.log('good evening');
+
+//Switch
+let shirt = 'shirt';
+
+switch (shirt) {
+    case 't-shirt':
+        console.log('faramarz wear red t-shirt');
+        break;
+    case 'shirt':
+        console.log('faramarz wear blue shirt');
+        break;
+    default:
+        break;
+}
+
+// write that with if else 
+// unlike java that people often use switch hear in javascript more like using if else
+
+if (shirt === 't-shirt')
+    console.log('faramarz wear red t-shirt');
+else if (shirt === 'shirt')
+console.log('faramarz wear blue shirt');
