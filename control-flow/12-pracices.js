da04e1f59d4ae7ba7e975e985019f56107682073
faramarz <37065807+newlighty@users.noreply.github.com>
2021-06-21 02:11:28 +0430

    end-of for

--- control-flow/12-pracices.js
+++ control-flow/12-pracices.js
@@ -1,32 +1,61 @@
 // Hour 
 //if else
-const Hour = 10;
+// const Hour = 10;
 
-if (Hour >= 6 && Hour < 12) {
-    console.log('good morning');
-} else if (Hour >= 12 && Hour < 18) {
-    console.log('good afternoon');
-} else
-    console.log('good evening');
+// if (Hour >= 6 && Hour < 12) {
+//     console.log('good morning');
+// } else if (Hour >= 12 && Hour < 18) {
+//     console.log('good afternoon');
+// } else
+//     console.log('good evening');
 
-//Switch
-let shirt = 'shirt';
+// //Switch
+// let shirt = 'shirt';
 
-switch (shirt) {
-    case 't-shirt':
-        console.log('faramarz wear red t-shirt');
-        break;
-    case 'shirt':
-        console.log('faramarz wear blue shirt');
-        break;
-    default:
-        break;
-}
+// switch (shirt) {
+//     case 't-shirt':
+//         console.log('faramarz wear red t-shirt');
+//         break;
+//     case 'shirt':
+//         console.log('faramarz wear blue shirt');
+//         break;
+//     default:
+//         break;
+// }
 
-// write that with if else 
-// unlike java that people often use switch hear in javascript more like using if else
+// // write that with if else 
+// // unlike java that people often use switch hear in javascript more like using if else
 
-if (shirt === 't-shirt')
-    console.log('faramarz wear red t-shirt');
-else if (shirt === 'shirt')
-console.log('faramarz wear blue shirt');
+// if (shirt === 't-shirt')
+//     console.log('faramarz wear red t-shirt');
+// else if (shirt === 'shirt')
+//     console.log('faramarz wear blue shirt');
+
+
+/// for 
+
+// for (let i = 0; i < 5; i++) {
+//     console.log('hello world');
+
+// }
+
+
+// for (let i = 0; i< 20; i++){
+//     console.log('what the ...');
+// }
+
+
+for (let i = 1; i <= 5; i++) {
+    if (i % 2 !== 0) console.log(i);
+    
+ }
+
+ for (let i = 5; i >= 1; i--) {
+    if (i % 2 !== 0) console.log(i);
+    
+ }
+console.log('reverse number----------------')
+ for (let i = 50; i >= 1; i--) {
+    if (i % 2 !== 0) console.log(i);
+    
+ }
