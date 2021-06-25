5752cf309965bac50075d5b762a4b2991247d5a2
faramarz <37065807+newlighty@users.noreply.github.com>
2021-06-26 00:41:25 +0430

    end of fizzBuzz exercise

--- control-flow/12-pracices.js
+++ control-flow/12-pracices.js
@@ -198,11 +198,42 @@
 // return (a > b) ? a : b;
 
 /////////////challenge-2 ---- landscape of portrait
-console.log(isLandscape(800, 600));
+// console.log(isLandscape(800, 600));
 
-function isLandscape(width, height) {
-return (width > height) ; // we use this instead of if else or ternary operation; because we just need true or false
-}
+// function isLandscape(width, height) {
+// return (width > height) ; // we use this instead of if else or ternary operation; because we just need true or false
+// }
+
+/////////////////////////// exercise 3 - fizzBuzz 
+
+// Divisible by 3 => Fizz 
+// Divisible by 5 => Buzz
+// Divisible bt both 3 and 5 => FizzBuzz 
+// not Divisible by 3 or 5 => input number
+// Not a number => 'Not number'
+
+
+// const output = fizzBuzz('16');
+// console.log(output);
+
+// function fizzBuzz(input) {
+//   if (typeof input !== 'number')
+//   return NaN;
+  
+//   if ((input % 3 === 0) && (input % 5 === 0))
+//      return 'FizzBuzz';
+
+//     if (input % 3=== 0)
+//     return 'Fizz';
+
+//     if (input % 5 === 0)
+//     return 'Buzz';
+
+
+//     return input;
+// }
+
+
 
 
 
