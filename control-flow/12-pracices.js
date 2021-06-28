580c42441b542e7bd90e8c1932c9d6a567fb7427
faramarz <37065807+newlighty@users.noreply.github.com>
2021-06-28 22:42:53 +0430

    exercise 9 grade

--- control-flow/12-pracices.js
+++ control-flow/12-pracices.js
@@ -354,4 +354,30 @@
 //     console.log(i);
     
 
+   ///////////// exercise 9 grade 
+
+   // const array = [100, 90, 50];
+
+   // function calculateGrade(marks) {
+   //    const average = calculateAverage(marks)
+
+   //    // if (average >= 0 && average <= 59)
+   //    //  return 'F';
+   //    if (average < 60) return 'F';
+   //    if (average < 70) return 'D';
+   //    if (average < 80) return 'C';
+   //    if (average < 90) return 'B';
+   //    return 'A';
+   // }
+
+   // // calculate average in separated reuseable function
+   // function calculateAverage(array) {
+   //    let sum = 0;
+   //    for (let value of array)
+   //       sum += value;
+   //    return  sum / array.length;
+   // }
+
+   // console.log(calculateGrade(array));
+
    
